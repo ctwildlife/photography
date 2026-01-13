b7fec749bd4e3490832b7ff5121052f94aa65602
ctwildlife <ctiernan@proton.me>
2026-01-13 17:47:16 -0500

    fixing menu

--- js-temp/load-nav.js
+++ js-temp/load-nav.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/photography/pages/nav.html")  // path to your nav HTML
+    fetch("/photography/includes/nav.html")  // path to your nav HTML
         .then(response => response.text())
         .then(data => {
             document.getElementById("navbar").innerHTML = data;
